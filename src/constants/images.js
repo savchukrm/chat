cd396167b0e01d046e1461934841fbd97b7d2da6
Rymma Savchuk <savchukrmm@gmail.com>
2023-07-06 10:37:14 +0300

    replaced background image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,4 @@
-import background from '../assets/images/background1.svg';
+import background from '../assets/images/BACKGROUND.svg';
 import logo from '../assets/images/logo.svg';
 import google from '../assets/images/google.svg';
 import userPhoto from '../assets/images/user-avatar.png';
