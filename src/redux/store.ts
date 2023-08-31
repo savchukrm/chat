51fccbf98eaf8eee58136a6314a8e128544375aa
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-31 15:38:03 +0300

    edits

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-
 import user from './user/slice';
 import modals from './modals/slice';
 import chats from './chat/slice';
