44520a9b05e74cb6fa6a15cdcb04769de99680c3
KarinaIvaschenko <karinka2323@gmail.com>
2023-08-31 16:15:52 +0300

    edits

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
+
 import user from './user/slice';
 import modals from './modals/slice';
 import chats from './chat/slice';
 import size from './size/slice';
 import languages from './languages/slice';
 import categories from './categories/slice';
-import activeChat from './activeChat/slice'
+import activeChat from './activeChat/slice';
 import allChats from './allChats/allChats';
 import filters from './filters/slice';
-
 
 export const store = configureStore({
   reducer: {
@@ -23,7 +23,6 @@ export const store = configureStore({
     allChats,
     filters,
   },
-
 });
 
 export type RootState = ReturnType<typeof store.getState>;
