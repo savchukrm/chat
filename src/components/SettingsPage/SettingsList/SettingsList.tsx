9dfd90845951d2418e70e9f7bf5e25ba7c5dd4db
KarinaIvaschenko <karinka2323@gmail.com>
2024-06-05 18:42:39 +0300

    settings list

--- src/components/SettingsPage/SettingsList/SettingsList.tsx
+++ src/components/SettingsPage/SettingsList/SettingsList.tsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
+
 const SettingsList = () => {
+  const [btnActive, setBtnActive] = useState<number | null>(0);
+
+  const selectSettings = (index: number) => {
+    setBtnActive(index);
+  };
+
+  const buttons = [
+    'My profile',
+    'Password',
+    'Notifications',
+    'Account actions',
+  ];
   return (
     <ul style={styles.listSettings}>
-      <li style={styles.itemSetting}>
-        <button style={styles.btnSetting}>My profile</button>
-      </li>
-      <li style={styles.itemSetting}>
-        <button style={styles.btnSetting}>Password</button>
-      </li>
-      <li style={styles.itemSetting}>
-        <button style={styles.btnSetting}>Notifications</button>
-      </li>
-      <li style={styles.itemSetting}>
-        <button style={styles.btnSetting}>Acount actions</button>
-      </li>
+      {buttons.map((buttonText, index) => (
+        <li key={index}>
+          <button
+            style={
+              btnActive === index
+                ? { ...styles.bntActive, ...styles.btnSetting }
+                : styles.btnSetting
+            }
+            onClick={() => selectSettings(index)}>
+            {buttonText}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
@@ -24,14 +39,19 @@ const styles = {
     flexDirection: 'column' as 'column',
     gap: '16px',
   },
-  itemSetting: {
+  bntActive: {
     backgroundColor: '#313339',
     borderRadius: 4,
+  },
+  btnSetting: {
+    color: '#fff',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
     paddingTop: 8,
     paddingBottom: 8,
     paddingLeft: 8,
   },
-  btnSetting: { color: '#fff', paddingTop: '4px', paddingBottom: '4px' },
 };
 
 export default SettingsList;
